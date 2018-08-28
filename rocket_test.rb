c718af1b1e0f1c37df9c7fb1a66323295ca9c019
Graham Spencer <grahamspence@gmail.com>
2018-08-28 16:54:30 -0400

    exercise complete i believe

--- rocket_test.rb
+++ rocket_test.rb
@@ -32,4 +32,44 @@ class RocketTest < Minitest::Test
     assert_equal(expected, actual)
   end
 
+  def test_colour_with_given_colour
+    @rocket = Rocket.new(colour: "red")
+    expected = "red"
+    actual = @rocket.colour
+    assert_equal(expected, actual)
+  end
+
+  def test_flying_equals_true
+    @rocket = Rocket.new(flying: true)
+    assert @rocket.flying?
+  end
+
+  def test_lift_off_true_when_rocket_not_flying
+    @rocket = Rocket.new(flying: false)
+    expected = true
+    actual = @rocket.lift_off
+    assert_equal(expected, actual)
+  end
+
+  def test_land_true_when_rocket_is_flying
+    @rocket = Rocket.new(flying: true)
+    expected = true
+    actual = @rocket.land
+    assert_equal(expected, actual)
+  end
+
+  def test_status_when_rocket_is_flying
+    @rocket = Rocket.new(name: "Gemini", flying: true)
+    expected = "Rocket Gemini is flying through the sky!"
+    actual = @rocket.status
+    assert_equal(expected, actual)
+  end
+
+  def test_status_when_rocket_is_not_flying
+    @rocket = Rocket.new(name: "Gemini", flying: false)
+    expected = "Rocket Gemini is ready for lift off!"
+    actual = @rocket.status
+    assert_equal(expected, actual)
+  end
+
 end
